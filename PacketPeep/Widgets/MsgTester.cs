6bce445b78248f2393f04a2916d55e4dbbb2beb6
Goomii <GoomiChan@users.noreply.github.com>
2021-10-27 13:33:13 +0100

    Some more work on the message tester

--- PacketPeep/Widgets/MsgTester.cs
+++ PacketPeep/Widgets/MsgTester.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
@@ -28,7 +29,7 @@ namespace PacketPeep.Widgets
         private TimeSpan profilePackMinTime;
         private TimeSpan profilePackMaxTime;
         private TimeSpan profilePackAvgTime;
-        
+
         private TimeSpan profileUnpackMinTime;
         private TimeSpan profileUnpackMaxTime;
         private TimeSpan profileUnpackAvgTime;
@@ -45,7 +46,6 @@ namespace PacketPeep.Widgets
         public void CalcData()
         {
             var headerData = Utils.GetGssMessageHeader(Msg);
-            //msgData = Msg.Data[headerData.Length..].ToArray();
 
             var tempBuffer = new byte[5000];
             getPackedSize    = MsgObj.GetPackedSize();
@@ -57,11 +57,34 @@ namespace PacketPeep.Widgets
             ProfilePackTime();
             ProfileUnPackTime();
 
-            CalcDiff();
-        }
+            var diffBytes = new List<HexView.HighlightSection>();
+            var msgBytes  = Msg.Data[headerData.Length..];
+            var maxBytes  = Math.Max(msgBytes.Length, msgData.Length);
 
-        private void CalcDiff()
-        {
+            for (int i = 0; i < maxBytes; i++) {
+                var highlight = new HexView.HighlightSection
+                {
+                    HoverName  = $"{i}",
+                    Offset     = i,
+                    Length     = 1,
+                    Color      = ImGui.ColorConvertU32ToFloat4(0xFF5057FF),
+                    IsSelected = false
+                };
+
+                if (i < msgBytes.Length && i < msgData.Length) {
+                    var byte1 = msgBytes[i];
+                    var byte2 = msgData[i];
+
+                    if (byte1 != byte2) {
+                        diffBytes.Add(highlight);
+                    }
+                }
+                else {
+                    diffBytes.Add(highlight);
+                }
+            }
+
+            hexDiff.SetData(msgData, diffBytes.ToArray());
         }
 
         private void ProfilePackTime()
@@ -95,9 +118,9 @@ namespace PacketPeep.Widgets
                     Channel.ReliableGss   => AeroMessageIdAttribute.MsgType.GSS,
                     Channel.UnreliableGss => AeroMessageIdAttribute.MsgType.GSS
                 };
-                
+
                 var msgObj = PacketParser.GetMessageFromIds(msgSrc, headerData.IsCommand ? AeroMessageIdAttribute.MsgSrc.Command : AeroMessageIdAttribute.MsgSrc.Message, headerData.MessageId, headerData.ControllerId);
-                
+
                 sw.Reset();
                 sw.Start();
                 var unpackedAmount = msgObj.Unpack(Msg.Data[headerData.Length..]);
@@ -112,13 +135,24 @@ namespace PacketPeep.Widgets
 
         public void Draw()
         {
-            ImGui.SetNextWindowSize(new Vector2(500, 300), ImGuiCond.Appearing);
+            ImGui.SetNextWindowSize(new Vector2(500, 0), ImGuiCond.Appearing);
             if (ImGui.Begin($"Message Tester {Utils.GetMessageName(Msg)}", ImGuiWindowFlags.NoSavedSettings)) {
                 if (ImGui.Button("Retest!", new Vector2(-1, 0))) {
                     CalcData();
                 }
 
                 DrawResultsTable();
+
+                if (!doesRepackMatch) {
+                    hexDiff.ShowSideParsedValues = true;
+                    hexDiff.ShowParsedValuesInTT = true;
+
+                    ImGui.PushID("Hex Diff View");
+                    ImGui.BeginChild("Hex Diff View 2", new Vector2(-1, 0), true, ImGuiWindowFlags.NoSavedSettings);
+                    hexDiff.Draw();
+                    ImGui.EndChild();
+                    ImGui.PopID();
+                }
             }
 
             ImGui.End();
@@ -137,11 +171,15 @@ namespace PacketPeep.Widgets
                 DrawResultTableRow("ActualPackedSize", $"{actualPackedSize}", msgData.Length == actualPackedSize);
 
                 DrawResultTableRow("Does Repack Match", $"{doesRepackMatch}", doesRepackMatch);
+                
+                DrawEmptyTableRow();
 
                 DrawResultTableRow("Unpack Timing Min", $"{profilePackMinTime.TotalMilliseconds} ms");
                 DrawResultTableRow("Unpack Timing Avg", $"{profilePackAvgTime.TotalMilliseconds} ms");
                 DrawResultTableRow("Unpack Timing Max", $"{profilePackMaxTime.TotalMilliseconds} ms");
                 
+                DrawEmptyTableRow();
+
                 DrawResultTableRow("Pack Timing Min", $"{profileUnpackMinTime.TotalMilliseconds} ms");
                 DrawResultTableRow("Pack Timing Avg", $"{profileUnpackAvgTime.TotalMilliseconds} ms");
                 DrawResultTableRow("Pack Timing Max", $"{profileUnpackMaxTime.TotalMilliseconds} ms");
@@ -169,5 +207,7 @@ namespace PacketPeep.Widgets
                 ThemeManager.PopFont();
             }
         }
+
+        private void DrawEmptyTableRow() => DrawResultTableRow("", "");
     }
 }
